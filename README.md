891a9238f86365f1af7f89edd5c52d63d784e77d
Mahmoud Sayed <mahmoudsayed0155@gmail.com>
2018-12-29 03:34:59 +0200

    README &seeds

--- README.md
+++ README.md
@@ -5,6 +5,16 @@ learning & playing around with GraphQL.
 ## Guide
 https://www.howtographql.com/graphql-ruby/1-getting-started/
 
+## Prerequisites
+- Ruby 2.4.1
+- Rails 5.2.2
+
+## Setup
+- install bundles, `bundle install`
+- setup database, `rake db:create db:migrate db:seed`
+- run server, `rails server -p3000`
+- go to http://localhost:3000/graphiql
+
 ## TODO
 - authenticate with JWT
 - authorize with cancancan/action_policy/pundit
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+ActiveRecord::Base.transaction do
+  [Vote, Link, User].each(&:destroy_all)
+
+  5.times do |i|
+    user = User.create! do |u|
+      u.name      = "User_#{i}"
+      u.email     = "example_#{i}@email.com"
+      u.password  = "SimplePa$$word"
+      u.password_confirmation  = "SimplePa$$word"
+    end
+
+    2.times do |j|
+      link = Link.create! do |l|
+        l.url = "http://www.website#{i*2 + j}.com"
+        l.description = "description #{i*2 + j}"
+        l.user = user
+      end
+    end
+  end
+
+  10.times do |i|
+    user = User.order(Arel.sql("RANDOM()")).first
+    link = Link.order(Arel.sql("RANDOM()")).first
+
+    Vote.create!(link: link, user: user)
+  end
+end
